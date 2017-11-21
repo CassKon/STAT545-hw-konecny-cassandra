2aa2b1935bd22a9db959c5634e4d87fa173ecf86
CassKon <konecny@zoology.ubc.ca>
2017-11-21 12:41:43 -0800

    Changes to app code

--- hw-8/BC_Liq_App/ui.R
+++ hw-8/BC_Liq_App/ui.R
@@ -1,5 +1,5 @@
 fluidPage(
-  titlePanel(title=div(img(src="BC_Liquor_Store_logo.png", height = 50), "BC Liquor Store Prices")),
+  titlePanel(title=div(img(src="BC_Liquor_Store_logo.png", height = 50)), windowTitle = "BC Liquor Store Prices"),
   sidebarLayout(
     sidebarPanel(
       sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
